d1b4085dbfd96115293807ad4dccc55ad4f35363
edgarwu102999508 <102999508@student.swin.edu.au>
2020-08-17 15:54:02 +1000

    Implementation

--- MiscLib/Group2.cs
+++ MiscLib/Group2.cs
@@ -1,34 +1,57 @@
 ﻿using System;
 
-namespace MiscLib
-{
-    public class GroupTwoFunctions
-    {
+namespace MiscLib {
+    public class GroupTwoFunctions {
         //---- Group 2 Functions ----------
-        public int CalcRectPerimeter(int length, int width) {
-            throw new NotImplementedException();
+        public int CalcRectPerimeter (int length, int width) {
+            return (length + width) * 2;
         }
 
-        public bool IsTriangularNum(int num) {
-            throw new NotImplementedException();
+        public bool IsTriangularNum (int num) {
+            if (num < 0)
+                return false;
+
+            int sum = 0;
+            for (int n = 1; sum <= num; n++) {
+                sum = sum + n;
+                if (sum == num)
+                    return true;
+            }
+            return false;
         }
 
-        public int FarenheitToCelcius(int farenheit) {
-            throw new NotImplementedException();
+        public int FarenheitToCelcius (int farenheit) {
+            return Math.Round ((farenheit - 32) * 5 / 9);
         }
 
-        public bool CheckWhiteSpace(string word) {
-            throw new NotImplementedException();
+        public bool CheckWhiteSpace (string word) {
+            word.ToCharArray ();
+            if (word[1] = " " && word[word.Length - 1] = " ") {
+                return true;
+            } else {
+                return false;
+            }
         }
 
-        public string CamelString(string word) {
-            throw new NotImplementedException();
+        public string CamelString (string word) {
+            word.ToCharArray ();
+            for (int i = 0; i < word.Length; i++) {
+                if (i % 2 == 0) {
+                    word[i].toUpper ();
+                } else {
+                    word[i].toLower ();
+                }
+            }
         }
 
-        public int CountDigits(string word) {
-            throw new NotImplementedException();
+        public int CountDigits (string word) {
+            for (int i; i < word.Length; i++) {
+                if (Char.IsDigit (word[i])) {
+                    int count = 0;
+                    count = count + i;
+                }
+            }
         }
 
-        
     }
 }
